3bfb5e1398f03ecaf31d48e4faa3b83496470020
Vinicius Gadelha <vinicius@j4s.tech>
2021-11-25 13:16:36 -0300

    produtos list

--- src/DAO/ProdutosDAO.java
+++ src/DAO/ProdutosDAO.java
@@ -1,17 +1,62 @@
 package DAO;
 
 
+import entities.Categoria;
+import entities.Marca;
 import entities.Produto;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProdutosDAO implements IProduto<Produto>{
 
-
     @Override
     public List<Produto> pesquisar(String search) {
+        java.sql.Connection conn = null;
+        try {
+            String sql ="SELECT preco, quantidade, produto.nome, produto.descricao, produto.tamanho, produto.id AS `id_produto`, marca.nome AS `marca`, categoria.nome AS `categoria`\n" +
+                    "  FROM estoque\n" +
+                    "  INNER JOIN produto ON estoque.id_produto = produto.id\n" +
+                    "  INNER JOIN marca ON produto.id_marca = marca.id\n" +
+                    "  INNER JOIN categoria ON produto.id_categoria = categoria.id\n" +
+                    "  WHERE produto.nome LIKE '%?%' OR produto.descricao LIKE '%?%'";
+            conn = Connection.getConnection();
+            PreparedStatement pstm = conn.prepareStatement(sql);
+            pstm.setString(1, search);
+            pstm.setString(2, search);
 
-        return null;
+            ResultSet rs = pstm.executeQuery();
+
+            List<Produto> produtos = new ArrayList<>();
+            while(rs.next()){
+
+                var prod = new Produto();
+                prod.setPreco(rs.getDouble("preco"));
+                prod.setQuantidade(rs.getInt("quantidade"));
+                prod.setNome(rs.getString("nome"));
+                prod.setDescricao(rs.getString("descricao"));
+                prod.setTamanho(rs.getString("tamanho"));
+                prod.setId(rs.getInt("id_produto"));
+
+                var marca = new Marca();
+                marca.setNome(rs.getString("marca"));
+                prod.setMarca(marca);
+
+                var categoria = new Categoria();
+                categoria.setNome(rs.getString("categoria"));
+                prod.setCategoria(categoria);
+
+                produtos.add(prod);
+            }
+            return produtos;
+        } catch (Exception e) {
+            System.err.println(e.getMessage()+"\n\n"+e.getStackTrace());
+            return null;
+        }finally{
+            Connection.closeConnection(conn);
+        }
     }
 
 }
--- src/control/ProdutosControl.java
+++ src/control/ProdutosControl.java
@@ -1,33 +1,17 @@
-//package control;
-//
-//import DAO.ProdutosDAO;
-//import entities.Roupa;
-//
-//import java.util.List;
-//
-//public class ProdutosControl {
-//
-//
-//    ProdutosDAO control = new ProdutosDAO();
-//
-//    public void Adicionar(Roupa roupa) {
-//        if (roupa != null) {
-//            control.adicionar(roupa);
-//        }
-//    }
-//
-//    public List<Roupa> pesquisar() {
-//        return null;
-//    }
-//
-//    public Roupa pesquisarPorNome(String nome) {
-//        if (nome != null) {
-//            Roupa roupa = new Roupa();
-//            roupa = control.pesquisar(nome);
-//            return roupa;
-//        } else {
-//            return null;
-//        }
-//    }
-//
-//}
+package control;
+
+import DAO.ProdutosDAO;
+import entities.Produto;
+
+import java.util.List;
+
+public class ProdutosControl {
+
+    ProdutosDAO dao = new ProdutosDAO();
+
+    public List<Produto> pesquisar(String search){
+
+        return dao.pesquisar(search);
+    }
+
+}
